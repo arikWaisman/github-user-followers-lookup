e97bc64a9e4232ed01c5d71b02f9906ad11b2864
Arik Waisman <awaisman@observer.com>
2017-11-14 17:31:44 -0500

    setting api url in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,16 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="csrf-token" value="<?php echo csrf_token(); ?>" />
+    <meta name="csrf-token" content="<?php echo csrf_token(); ?>" />
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <title>Github Follower Search</title>
 
 </head>
 <body>
     <div id="hook"></div>
+    <script type="text/javascript">
+        const MYAPI_URL = "{{  url('/papi') }}";
+    </script>
     <script type="text/javascript" src="{{ asset('/js/app.js') }}"></script>
 </body>
 </html>
